f5c49a1b99cd1b6d8956b7729d107c4a25702c9e
Patryk Slawinski-Slawecki <p.slawinski.s@gmail.com>
2022-08-08 17:21:59 +0200

    Division operation added

--- Calculator.py
+++ Calculator.py
@@ -2,7 +2,8 @@ print("SIMPLE CALCULATOR")
 operation = input("Choose operation:\n"
                   "1. Addition:\n"
                   "2. Subtraction:\n"
-                  "3. Multiplication:\n")
+                  "3. Multiplication:\n"
+                  "4. Division\n")
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 if operation == "1":
@@ -13,6 +14,9 @@ elif operation == "2":
     print("Result: ", result)
 elif operation == "3":
     result = num1 * num2
+    print("Result: ", result)
+elif operation == "4":
+    result = num1 / num2
     print("Result: ", result)
 else:
     print("Wrong operation!")
