c77bd9d228f80244be0b478c418168604992d496
Patryk Slawinski-Slawecki <p.slawinski.s@gmail.com>
2022-08-08 17:20:14 +0200

    Multiplication operation added

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,8 @@
 print("SIMPLE CALCULATOR")
 operation = input("Choose operation:\n"
                   "1. Addition:\n"
-                  "2. Subtraction:\n")
+                  "2. Subtraction:\n"
+                  "3. Multiplication:\n")
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 if operation == "1":
@@ -9,6 +10,9 @@ if operation == "1":
     print("Result: ", result)
 elif operation == "2":
     result = num1 - num2
+    print("Result: ", result)
+elif operation == "3":
+    result = num1 * num2
     print("Result: ", result)
 else:
     print("Wrong operation!")
